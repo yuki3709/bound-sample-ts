eb164c0f3a7d8056381717f951b627639eaeccc0
yuki3709 <32707335+yuki3709@users.noreply.github.com>
2018-06-26 05:02:32 +0900

    fix Initial Value

--- src/Field.ts
+++ src/Field.ts
@@ -4,14 +4,17 @@ export class Field {
     canvas: HTMLCanvasElement;
     context: CanvasRenderingContext2D;
     size: {
-        width: 0,
-        height: 0
+        width: number,
+        height: number
     };
     components: Component[];
+
     constructor(e) {
         this.canvas = e;
         this.context = this.canvas.getContext('2d');
         this.context.globalCompositeOperation = "source-over";
+        this.size = { width: 0, height: 0 };
+        this.components = [];
         setInterval(() => this.draw(), 33);
     }
 
@@ -22,15 +25,23 @@ export class Field {
         context.fillStyle = "#000";
         context.fillRect(0, 0, size.width, size.height);
     }
+
     resize(parent) {
-        this.size.width = this.canvas.width = parent.clientWidth;
-        this.size.height = this.canvas.height = parent.clientHeight;
+        const width = parent.clientWidth;
+        const height = parent.clientHeight;
+        this.size.width = width;
+        this.canvas.width = width;
+        this.size.height = height;
+        this.canvas.height = height;
     }
+
     add(component) {
         this.components.push(component);
     }
+
     draw() {
         this.clear();
+        if (this.components.length < 1) return;
         this.components.forEach(component => {
             component.move(this.size);
             component.draw(this.context);
